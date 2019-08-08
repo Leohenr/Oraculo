844600071cc3c6ae6f2b4c96351505c80cfe6011
Leonardo <leo_henr@hotmail.com>
2019-08-07 21:49:34 -0300

    First Commit

---
+++ Oraculo/Oraculo/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Oraculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = ConnectionMultiplexer.Connect("40.77.24.62");
+            var db = connection.GetDatabase();
+            var sub = connection.GetSubscriber();
+            sub.Subscribe("Perguntas", (ch, msg) =>
+            {
+                Console.WriteLine($"{DateTime.Now.ToString("dd/MM/yyy - HH:mm:ss")}<Normal - {ch}><{msg}>.");
+            });
+            Console.ReadLine();
+        }
+    }
+}
