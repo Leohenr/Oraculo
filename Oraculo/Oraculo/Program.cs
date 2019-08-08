ec77348f2bb38cc451b9441eabc6927ab27a368a
Leonardo <leo_henr@hotmail.com>
2019-08-07 22:27:47 -0300

    Funcionou 1

--- Oraculo/Oraculo/Program.cs
+++ Oraculo/Oraculo/Program.cs
@@ -10,12 +10,17 @@ namespace Oraculo
             var connection = ConnectionMultiplexer.Connect("40.77.24.62");
             var db = connection.GetDatabase();
             var sub = connection.GetSubscriber();
-            
-            sub.Subscribe("Perguntas", (ch, msg) =>
+            while (true)
             {
-                Console.WriteLine(msg.ToString());
-            });
-            Console.ReadLine();
+                sub.Subscribe("Perguntas", (ch, msg) =>
+                {
+                    Console.WriteLine(msg.ToString());
+                    string resposta = Console.ReadLine();
+
+                    var perguntaNumero = msg.ToString().Substring(0, msg.ToString().IndexOf(":"));
+                    db.HashSet(perguntaNumero, new HashEntry[] { new HashEntry("EstouTestandoEin", resposta) });
+                });
+            }
         }
     }
 }
